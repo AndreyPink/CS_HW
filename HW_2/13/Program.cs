3b2b6a1709b0570c2a7101edd39279444446b322
ANDREY PINK <andreypink@bk.ru>
2022-12-02 14:30:48 +0300

    update 13

--- HW_2/13/Program.cs
+++ HW_2/13/Program.cs
@@ -6,12 +6,15 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+
 Console.WriteLine("Введите скольки-хочешь-значное число");
-int num = int.Parse(Console.ReadLine()!);
+//поскольку в условии не указано иное, 
+//код позволяет проверять в том числе отрицательные числа
+int num = Math.Abs(int.Parse(Console.ReadLine()!));
 int numTemp = num;
 int count = 0;
 
-while (Math.Abs(numTemp) > 0)   //код позволяет проверять отрицательные числа
+while (numTemp > 0)   
 {
   numTemp /= 10;
   count++; 
@@ -21,7 +24,7 @@ if (count - 3 >= 0)
 {
     var rank = Math.Pow(10, count - 3);
     var num3 = Math.Truncate(num / rank % 10);
-    Console.WriteLine($"Третья цифра числа {num} = {Math.Abs(num3)}");
+    Console.WriteLine($"Третья цифра {num3}");
 }
 else
 {
