a0a13021c5078c1625564db68dc909973955e7de
ANDREY PINK <andreypink@bk.ru>
2022-12-06 11:49:26 +0300

    add new HW

---
+++ HW_3/19/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите слово:");
+string text = Console.ReadLine()!;
+int len = text.Length;
+if(text.Length % 2 == 0)
+{
+    string text1 = text.Substring(0, len / 2);
+    string text2 =  text.Substring(len / 2, len / 2);
+    string text2rev = new string(text2.ToCharArray().Reverse().ToArray());
+    if(text1 == text2rev)
+    {
+        Console.WriteLine($"Слово {text} Является палиндромом");
+    }
+    else {Console.WriteLine($"Слово {text} Не является палиндромом");}
+}
+else
+{
+    string text1 = text.Substring(0, (len - 1) / 2);
+    string text2 =  text.Substring((len - 1) / 2 + 1, (len - 1) / 2);
+    string text2rev = new string(text2.ToCharArray().Reverse().ToArray());
+    if(text1 == text2rev)
+    {
+        Console.WriteLine($"Слово {text} Является палиндромом");
+    }
+    else {Console.WriteLine($"Слово {text} Не является палиндромом");}
+}
+
