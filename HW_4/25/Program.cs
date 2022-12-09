a52ba6f941d4a122cdf874cf790cce8eeded93aa
ANDREY PINK <andreypink@bk.ru>
2022-12-09 12:08:28 +0300

    add New HW

--- HW_4/25/Program.cs
+++ HW_4/25/Program.cs
@@ -3,3 +3,15 @@
 // в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+int NumPow(int A, int B)
+{
+     
+    int pow = (int)Math.Pow(A, B);
+    return pow;
+}
+Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write($"A в степени В = {NumPow(a, b)}");
