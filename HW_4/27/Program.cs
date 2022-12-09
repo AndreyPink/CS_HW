7fb1f36b8fa97668a53cb6de93cbf9f21baa666f
ANDREY PINK <andreypink@bk.ru>
2022-12-09 11:56:32 +0300

    add New HW

--- HW_4/27/Program.cs
+++ HW_4/27/Program.cs
@@ -3,3 +3,18 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int CountNum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int x =num%10;
+        num /= 10;
+        sum += x;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма цифр в числе {n} = {CountNum(n)}");
