2c0e4bedb7f7fc64a779ab493d8a99ea44a201e6
ANDREY PINK <andreypink@bk.ru>
2022-12-24 01:50:38 +0300

    add HW_9

--- HW_9/66/Program.cs
+++ HW_9/66/Program.cs
@@ -3,8 +3,10 @@
 // в промежутке от M до N. 
 // Выполнить с помощью рекурсии.
 
+
 int SumElem(int m, int n)
 {
-return n > m ? n + SumElem(m, n - 1) : n;
+if(n > m) return n > m ? n + SumElem(m, n - 1) : n;
+else return m > n ? m + SumElem(m - 1, n) : m;
 }
 Console.WriteLine(SumElem(4, 8));
